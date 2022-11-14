1fd39a606b009c563865de14691c4f9387488972
Jonathan Jester <jalanjester06@gmail.com>
2022-11-13 21:51:31 -0700

    Added BuildFamilyRelationships

---
+++ FamilyMemories/Controllers/UpdateFamMemberRelationshipsController.cs
@@ -0,0 +1,41 @@
+﻿using FamilyMemories.Data;
+using FamilyMemories.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FamilyMemories.Controllers
+{
+    public class UpdateFamMemberRelationshipsController : Controller
+    {
+        private readonly FamilyMembersDbContext _familyMembersDbContext;
+        public UpdateFamMemberRelationshipsController(FamilyMembersDbContext context)
+        {
+            _familyMembersDbContext = context;
+        }
+        [HttpGet]
+        public IActionResult Index(int famMemId)
+        {
+            var self = _familyMembersDbContext.FamilyMembers.Find(famMemId);
+            var vm = new FamilyMemberRelationshipsViewModel()
+            {
+                Self = self,
+                Options = BindToSelectList()
+            };
+
+            return View(vm);
+        }
+
+        private List<SelectListItem> BindToSelectList()
+        {
+            List<SelectListItem> Options;
+            Options = _familyMembersDbContext.FamilyMembers.Select(x =>
+            new SelectListItem
+            {
+                Value = x.Id.ToString(),
+                Text = x.FirstName + " " + x.LastName
+
+            }).ToList();
+            return Options;
+        }
+    }
+}
